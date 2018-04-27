da8d38f615950ae4ed2bc839912e7ef767562879
Rehan <zrony58@gmail.com>
2018-04-28 02:16:02 +0500

    Initial commit

---
+++ Constructor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car=new Car("XYZ1234");
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Constructor/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Constructor
+{
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+
+        //        public Vehicle()
+        //        {
+        //            Console.WriteLine("Vehicle is being initialized.");
+        //        }
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
+            Console.WriteLine("Vehicle is being initialized. {0}", registrationNumber);
+        }
+
+
+    }
+}
